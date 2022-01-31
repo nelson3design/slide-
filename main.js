f25c01f6f72193abfd02dac35387ad08baf63d4c
nelson3design <mandeart2@gmail.com>
2022-01-31 14:58:41 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+var balls = document.querySelector('.balls')
+
+var quant = document.querySelectorAll('.slide .img')
+
+var atual=0;
+
+var imagem = document.getElementById('atual')
+
+var voltar = document.getElementById('voltar')
+
+var next = document.getElementById('next')
+var rolar= true
+
+// for(let i=0; i < quant.length; i++){
+//     var div = document.createElement('div')
+//     div.id=i
+//     balls.appendChild(div)
+// }
+
+document.getElementById('0').classList.add('imgAtual')
+
+var pos=document.querySelectorAll('.balls div')
+
+for(let i=0; i < pos.length; i++){
+    pos[i].addEventListener('click',()=>{
+        atual= pos[i].id
+        rolar=false
+        slide()
+    })
+}
+
+voltar.addEventListener('click',()=>{
+    atual--
+      rolar=false
+      slide()
+})
+
+next.addEventListener('click',()=>{
+    atual++
+      rolar=false
+      slide()
+})
+
+function slide(){
+    if(atual >= quant.length){
+        atual= 0
+    }else if(atual < 0){
+        atual= quant.length-1
+    }
+
+    document.querySelector('.imgAtual').classList.remove('imgAtual')
+    // imagem.style.marginLeft= -1024*atual+'px'
+
+    imagem.style.marginLeft= -100*atual+'vw'
+    document.getElementById(atual).classList.add('imgAtual')
+}
+
+
+setInterval(()=>{
+
+    if(rolar){
+
+        atual++
+        slide()
+    }else{
+        rolar=true
+    }
+},4000)
